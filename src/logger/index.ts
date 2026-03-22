f5bb33b2280f9c79b3f6fca0d08d98da84a00c20
Artjom Kurapov <artkurapov@gmail.com>
2026-03-22 11:16:17 +0200

    upgrade to @gratheon/log-lib v3 and adapt logger wrapper

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,62 +1,15 @@
-function log(level: string, message: string, meta?: any) {
-    let time = new Date().toISOString();
-    let hhMMTime = time.slice(11, 19);
-    meta = meta ? JSON.stringify(meta) : '';
+import { createLogger } from "@gratheon/log-lib";
 
-    // colorize time to have ansi blue color
-    hhMMTime = `\x1b[34m${hhMMTime}\x1b[0m`;
-
-    if (level === 'error') {
-        level = `\x1b[31m${level}\x1b[0m`;
-        meta = `\x1b[35m${meta}\x1b[0m`;
-    } else if (level === 'info') {
-        level = `\x1b[32m${level}\x1b[0m`;
-        meta = `\x1b[35m${meta}\x1b[0m`;
-    } else if (level === 'debug') {
-        level = `\x1b[90m${level}\x1b[0m`;
-        message = `\x1b[90m${message}\x1b[0m`;
-        meta = `\x1b[90m${meta}\x1b[0m`;
-    } else if (level === 'warn') {
-        level = `\x1b[33m${level}\x1b[0m`;
-        meta = `\x1b[35m${meta}\x1b[0m`;
-    }
-
-    console.log(`${hhMMTime} [${level}]: ${message} ${meta}`);
-}
+const { logger: baseLogger } = createLogger();
 
 export const logger = {
-    log: (message: string, meta?: any) => {
-        log('info', message, meta)
-    },
-    info: (message: string, meta?: any) => {
-        log('info', message, meta)
-    },
-    error: (message: string | Error | any, meta?: any) => {
-        if (message.message && message.stack) {
-            return log('error', message.message, {
-                stack: message.stack,
-                ...meta
-            });
-        }
-        log('error', String(message), meta)
-    },
-    errorEnriched: (message: string, error: Error|any, meta?: any) => {
-        if (error.message && error.stack) {
-            return log('error', `${message}: ${error.message}`, {
-                stack: error.stack,
-                ...meta
-            });
-        }
-        log('error', String(message), meta)
-    },
-    warn: (message: string, meta?: any) => {
-        log('warn', message, meta)
-    },
-
-    // do not store debug logs in DB
-    debug: (message: string, meta?: any) => {
-        log('debug', message, meta)
-    },
+    log: (message: string, meta?: any) => baseLogger.info(message, meta as any),
+    info: (message: string, meta?: any) => baseLogger.info(message, meta as any),
+    warn: (message: string, meta?: any) => baseLogger.warn(message, meta as any),
+    debug: (message: string, meta?: any) => baseLogger.debug(message, meta as any),
+    error: (message: string | Error | any, meta?: any) => baseLogger.error(message, meta as any),
+    errorEnriched: (message: string, error: Error | any, meta?: any) =>
+        baseLogger.errorEnriched(message, error, meta as any),
 };
 
 
