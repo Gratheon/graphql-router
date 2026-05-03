c44250977e0bf92df951d0ba65f44e7c25113cb0
Artjom Kurapov <artkurapov@gmail.com>
2026-05-04 02:13:36 +0300

    ...

--- src/config/config.prod.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	redisHost: process.env.REDIS_HOST || '127.0.0.1',
-	redisPort: parseInt(process.env.REDIS_PORT || '6379', 10),
-	redisSecret: process.env.REDIS_SECRET || process.env.REDIS_PASSWORD || 'pass',
-	schemaRegistryUrl: process.env.SCHEMA_REGISTRY_URL || 'http://127.0.0.1:3000',
-	userCycleUrl: process.env.USER_CYCLE_URL || 'http://127.0.0.1:4000',
-};
